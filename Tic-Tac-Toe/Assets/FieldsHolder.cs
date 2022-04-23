f44762b4a4639a99e7b230a165de524b9c787b8e
mmydlak <marcinmydlak97@gmail.com>
2022-04-23 05:24:09 +0200

    fields positioning fixed

--- Tic-Tac-Toe/Assets/FieldsHolder.cs
+++ Tic-Tac-Toe/Assets/FieldsHolder.cs
@@ -9,17 +9,32 @@ public class FieldsHolder : MonoBehaviour
     // 4 5 6
     // 1 2 3
     private Transform[] fields;
+    private Vector3[] initialPositions = {
+        new Vector3(-5.5f, 0.1f, -5.5f),
+        new Vector3(0.0f, 0.1f, -5.5f),
+        new Vector3(5.5f, 0.1f, -5.5f),
+
+        new Vector3(-5.5f, 0.1f, 0.0f),
+        new Vector3(0.0f, 0.1f, 0.0f),
+        new Vector3(5.5f, 0.1f, 0.0f),
+
+        new Vector3(-5.5f, 0.1f, 5.5f),
+        new Vector3(0.0f, 0.1f, 5.5f),
+        new Vector3(5.5f, 0.1f, 5.5f)
+    };
     void Start()
     {
-        fields = new Transform[9];
+        this.fields = new Transform[9];
         for (int i=0; i<9; i++)
         {
             Transform field = this.transform.Find("Field"+(i+1));
-            fields[i] = field;
+            field.position = this.initialPositions[i];
+            this.fields[i] = field;
         }
     }
 
     public IList<Transform> getFields() {
-        return Array.AsReadOnly(this.fields);
+        Debug.Log(this.fields);
+        return Array.AsReadOnly<Transform>(this.fields);
     }
 }
